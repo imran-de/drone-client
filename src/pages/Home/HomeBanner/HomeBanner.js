3c5f90e43286b2bef229a55f430515bc2b81cffd
imran-de <stimran4405@gmail.com>
2021-11-10 10:09:20 +0600

    navigation

--- src/pages/Home/HomeBanner/HomeBanner.js
+++ src/pages/Home/HomeBanner/HomeBanner.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
 import TopNavigation from '../../Shared/TopNavigation/TopNavigation';
 
 const HomeBanner = () => {
     return (
         <div>
             <TopNavigation />
+            <Navigation />
         </div>
     );
 };
--- src/pages/Shared/Navigation/Navigation.js
+++ src/pages/Shared/Navigation/Navigation.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-
+//import css file
+import './Navigation.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+// logo import
+import logo from '../../../Images/drone.png';
 const Navigation = () => {
     return (
-        <Container>
-            <h2>This is container header menu bar</h2>
-        </Container>
+        <Navbar bg="light" expand="lg">
+            <Container className="header">
+                <Navbar.Brand as={Link} to="/"><img className="site-logo" src={logo} alt="The drone logo" /> Drone</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
