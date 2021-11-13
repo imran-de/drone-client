03ccda4563ce440d389694d0a57f339c149aa310
imran-de <stimran4405@gmail.com>
2021-11-13 12:17:26 +0600

    spinner

--- src/pages/Shop/Products/Products.js
+++ src/pages/Shop/Products/Products.js
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './Products.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Spinner } from 'react-bootstrap';
 import Product from '../Product/Product';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
+        setLoading(true);
         fetch(`https://imran-drone.herokuapp.com/products`)
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data)
+                setLoading(false)
+            })
     }, [])
     return (
         <Container className="my-5">
             <Row xs={1} md={2} className="g-4">
+                {/* preloader */}
+                {loading && <div className="d-flex justify-content-center align-items-center">
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>}
                 <Col sm={12} md={10} lg={8}>
                     <div className="products-container">
                         {
