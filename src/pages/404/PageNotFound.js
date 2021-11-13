b60358de3cfb0cc5674dce15e43ada24f8852cdb
imran-de <stimran4405@gmail.com>
2021-11-13 08:53:50 +0600

    404 upadte

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import Navigation from '../Shared/Navigation/Navigation';
+import PageBanner from '../Shared/PageBanner/PageBanner';
+import Footer from '../Shared/Footer/Footer';
+import { useHistory } from 'react-router';
 
 const PageNotFound = () => {
+    const history = useHistory();
+
     return (
-        <Container>
-            <Navigation />
-            <h2 className="my-5">The page you are looking not found!</h2>
-        </Container>
+        <>
+            <PageBanner pageTitle="Not Found Page"></PageBanner>
+            <Container className="my-5 position-relative">
+                <div className="d-flex justify-content-center position-relative">
+                    <img className="img-fluid" src="https://i.ibb.co/xKbGm4J/error.png" alt="" />
+                    <div className="text-center" style={{ position: 'absolute', bottom: '0px' }}>
+                        <h2 className="fs-md-1 blue">Oops... It looks like you 're lost !</h2>
+                        <p>The page you were looking for dosen't exist.</p>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <button onClick={() => history.go(-1)} className="purchase">GO BACK</button>
+                </div>
+            </Container>
+
+            <Footer />
+        </>
     );
 };
 
